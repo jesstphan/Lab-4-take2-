ee25d443459e7fa558b4eb5927a2d6abbb6914fa
jesstphan <92838597+jesstphan@users.noreply.github.com>
2021-10-20 14:44:28 -0400

    Update LEDControl.py

--- LEDControl.py
+++ LEDControl.py
@@ -4,16 +4,6 @@ import json
 import cgitb
 cgitb.enable()
 
-ledPin1 = 19
-ledPin2 = 20
-ledPin3 = 21
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False) # ignore warnings due to multiple scripts at same time
-GPIO.setup(ledPin1, GPIO.OUT)
-GPIO.setup(ledPin2, GPIO.OUT)
-GPIO.setup(ledPin3, GPIO.OUT)
-
 data = cgi.FieldStorage()
 s1 = data.getvalue('slider1')
 b = data.getvalue('option')
@@ -24,7 +14,12 @@ with open('ledpwm.txt', 'w') as f:
    
 print('Content-type: text/html\n\n')
 print('<html>')
-print('<form action="/cgi-bin/LED_Control.py" method="POST">')
+print('<form action="/cgi-bin/LEDControl.py" method="POST">')
+if b == 'g':
+  print('input type="radio" name="option" value="b" checked> LED Blue <br>')
+ 	print('<input type="radio" name="option" value="g"> LED Green <br>')
+ 	print('<input type="radio" name="option" value="w"> LED White <br>')
+  
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
 print('<input type="submit" value="Change LED brightness">')
 print('</form>')
